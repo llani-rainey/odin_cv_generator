9dd13dc5f6c4980209505d10a557a2e99bd8c0f9
Llani <llani.rainey@gmail.com>
2026-04-07 18:41:42 +0100

    Fix insertion of sections to handle deleted education and work experience

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),  # must be before api/
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/', include('allauth.urls')),
     path('api/', include('cv_api.urls')),
 ]
